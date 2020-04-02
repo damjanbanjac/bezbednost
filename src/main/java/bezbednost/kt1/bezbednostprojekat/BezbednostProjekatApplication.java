587216264427448e2be2086842859539a2f26734
damjanbanjac <damjanbanjac14@gmail.com>
2020-04-02 18:44:01 +0200

    Inicijalizacija projekta

---
+++ src/main/java/bezbednost/kt1/bezbednostprojekat/BezbednostProjekatApplication.java
@@ -0,0 +1,13 @@
+package bezbednost.kt1.bezbednostprojekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezbednostProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezbednostProjekatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bezbednost/kt1/bezbednostprojekat/BezbednostProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package bezbednost.kt1.bezbednostprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezbednostProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
